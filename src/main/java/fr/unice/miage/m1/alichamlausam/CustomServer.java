1db82d492c2c5cfa9ef6fc5a64fabad3ac4bc4f4
Alison Temin <alison.temin@etu.unice.fr>
2018-01-04 15:30:40 +0100

    Serveur Jetty

---
+++ src/main/java/fr/unice/miage/m1/alichamlausam/CustomServer.java
@@ -0,0 +1,49 @@
+package fr.unice.miage.m1.alichamlausam;
+
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class CustomServer {
+
+    public static void main(String[] args) {
+
+        ResourceConfig config = new ResourceConfig();
+        config.packages("fr.unice.miage.m1.alichamlausam");
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+
+        Server server = new Server(2222);
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+
+//        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//        context.setContextPath("/");
+
+        /*Server server = new Server(2222);
+        server.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        // Tells the Jersey Servlet which REST service/class to load.
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                CentreDeRechercheService.class.getCanonicalName());
+*/
+        try {
+            server.start();
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //server.destroy();
+        }
+    }
+}
