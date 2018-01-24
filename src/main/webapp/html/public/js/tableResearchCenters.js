9bfc79bc7027aec634f12d15d465e74a887d22d6
ablohadeola <ablohadeola@gmail.com>
2018-01-24 22:15:06 +0100

    centre de recherche fonctionne

--- src/main/webapp/html/public/js/tableResearchCenters.js
+++ src/main/webapp/html/public/js/tableResearchCenters.js
@@ -4,7 +4,7 @@ function fillCentresTable() {
 
     //Appel ajax
     //http:localhost:2222/centrederecherche/details
-    fetch("http://localhost:2222/centrederecherche/details").then(function (reponse) {
+    fetch("http://localhost:2222/rest/centrederecherche/details").then(function (reponse) {
         return reponse.text();
     }).then(function (text) {
         var dp = new DOMParser();
@@ -35,7 +35,7 @@ function fillCentresTable() {
 
     //Appel ajax
     //http:localhost:2222/centrederecherche/personnes
-    fetch("http://localhost:2222/centrederecherche/personnes").then(function (reponse) {
+    fetch("http://localhost:2222/rest/centrederecherche/personnes").then(function (reponse) {
         return reponse.text();
     }).then(function (text) {
         var dp = new DOMParser();
